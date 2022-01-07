04f40ecf8dd1006612f070c5a96236a4370d3923
jun50 <39844671+jun50@users.noreply.github.com>
2022-01-08 00:27:27 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.id == 508166063473688577 and message.embeds[0].author.name == "タイピングゲーム":
+    if message.author.id == 508166063473688577 and message.embeds and message.embeds[0].author.name == "タイピングゲーム":
         await message.channel.send(message.embeds[0].description.split("\n")[-1].replace(" ", ""))
 
 client.run("token")
